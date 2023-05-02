9335ef9016f561fefd0d92f93fa8952322270ec4
Nikolas Klauser <n_klauser@apple.com>
2023-05-02 15:08:46 -0700

    [libc++][PSTL] Replace _PSTL_ASSERT with _LIBCPP_ASSERT
    Summary:
    
    Reviewers: ldionne
    
    Spies:
    
    Differential Revision: https://reviews.llvm.org/D149605

--- libcxx/include/__pstl/internal/parallel_backend_utils.h
+++ libcxx/include/__pstl/internal/parallel_backend_utils.h
@@ -10,6 +10,7 @@
 #ifndef _PSTL_PARALLEL_BACKEND_UTILS_H
 #define _PSTL_PARALLEL_BACKEND_UTILS_H
 
+#include <__assert>
 #include <iterator>
 #include <utility>
 #include "utils.h"
@@ -55,7 +56,7 @@ struct __serial_move_merge
         constexpr bool __same_move_seq = std::is_same<_MoveSequenceX, _MoveSequenceY>::value;
 
         auto __n = _M_nmerge;
-        _PSTL_ASSERT(__n > 0);
+        _LIBCPP_ASSERT(__n > 0, "");
 
         auto __nx = __xe - __xs;
         //auto __ny = __ye - __ys;
