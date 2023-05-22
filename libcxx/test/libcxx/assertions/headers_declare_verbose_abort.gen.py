9157b6a8eec26c2b35103bac4b29cdd7c50ac783
Louis Dionne <ldionne.2@gmail.com>
2023-05-22 12:47:00 -0700

    [libc++] Use the new .gen tests to implement __verbose_abort tests
    This reduces the amount of boilerplate that we need to generate
    for each commit. It also resolves a problem where the modular CI
    would run extremely slow on this test because we'd define a macro
    before including the standard library, defeating the module cache.
    
    Differential Revision: https://reviews.llvm.org/D151156

---
+++ libcxx/test/libcxx/assertions/headers_declare_verbose_abort.gen.py
@@ -0,0 +1,33 @@
+#===----------------------------------------------------------------------===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===----------------------------------------------------------------------===##
+
+# Test that all public C++ headers define the verbose termination function, which
+# is required for users to be able to include any public header and then override
+# the function using a strong definition.
+
+# XFAIL: availability-verbose_abort-missing
+
+# RUN: %{python} %s %{header-variables}
+
+import base64, pickle, sys
+vars = pickle.loads(base64.b64decode(sys.argv[1]))
+
+for header in vars['public_headers']:
+    # Skip C compatibility headers.
+    if header.endswith('.h'):
+        continue
+
+    test_condition_begin = f'#if {vars["header_restrictions"][header]}' if header in vars['header_restrictions'] else ''
+    test_condition_end = '#endif' if header in vars['header_restrictions'] else ''
+    print(f"""\
+//--- {header}.compile.pass.cpp
+{test_condition_begin}
+#include <{header}>
+using HandlerType = decltype(std::__libcpp_verbose_abort);
+{test_condition_end}
+""")
