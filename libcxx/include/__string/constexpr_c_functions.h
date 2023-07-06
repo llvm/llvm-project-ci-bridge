07379162c36ffc6373f03e602af2a390eeb6a9f8
Louis Dionne <ldionne.2@gmail.com>
2023-07-06 10:27:54 -0400

    [libc++] Fix std::move algorithm with trivial move-only types
    As reported in https://reviews.llvm.org/D151953#4472195, the std::move
    algorithm (and various other functions that relied on it) stopped working
    after starting to use `__constexpr_memmove` in its implementation. This
    patch fixes the underlying issue in `__constexpr_memmove` and adds tests
    for various related algorithms and functions that were not exercising
    trivial move-only types.
    
    Differential Revision: https://reviews.llvm.org/D154613

--- libcxx/include/__string/constexpr_c_functions.h
+++ libcxx/include/__string/constexpr_c_functions.h
@@ -10,15 +10,22 @@
 #define _LIBCPP___STRING_CONSTEXPR_C_FUNCTIONS_H
 
 #include <__config>
+#include <__memory/construct_at.h>
 #include <__type_traits/datasizeof.h>
+#include <__type_traits/integral_constant.h>
 #include <__type_traits/is_always_bitcastable.h>
 #include <__type_traits/is_constant_evaluated.h>
+#include <__type_traits/is_copy_assignable.h>
+#include <__type_traits/is_copy_constructible.h>
 #include <__type_traits/is_equality_comparable.h>
+#include <__type_traits/is_move_assignable.h>
+#include <__type_traits/is_move_constructible.h>
 #include <__type_traits/is_same.h>
 #include <__type_traits/is_trivially_copyable.h>
 #include <__type_traits/is_trivially_lexicographically_comparable.h>
 #include <__type_traits/remove_cv.h>
 #include <__utility/is_pointer_in_range.h>
+#include <__utility/move.h>
 #include <cstddef>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
@@ -133,6 +140,56 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 _Tp* __constexpr_memchr(_Tp*
   }
 }
 
+// This function performs the assignment of an existing, already alive TriviallyCopyable object.
+// It basically works around the fact that TriviallyCopyable objects are not required to be
+// syntactically copy/move constructible or copy/move assignable. Technically, only one of the
+// four operations is required to be syntactically valid -- but at least one definitely has to
+// be valid.
+//
+// This is necessary in order to implement __constexpr_memmove below in a way that mirrors as
+// closely as possible what the compiler's __builtin_memmove is able to do.
+template <class _Tp> // copy assignment
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 _Tp&
+__assign_trivially_copyable_impl(_Tp* __dest, _Tp&& __src, integral_constant<int, 0>) {
+  *__dest = __src;
+  return *__dest;
+}
+
+template <class _Tp> // move assignment
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 _Tp&
+__assign_trivially_copyable_impl(_Tp* __dest, _Tp&& __src, integral_constant<int, 1>) {
+  *__dest = std::move(__src);
+  return *__dest;
+}
+
+template <class _Tp> // copy constructor
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 _Tp&
+__assign_trivially_copyable_impl(_Tp* __dest, _Tp&& __src, integral_constant<int, 2>) {
+  std::__construct_at(__dest, __src);
+  return *__dest;
+}
+
+template <class _Tp> // move constructor
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 _Tp&
+__assign_trivially_copyable_impl(_Tp* __dest, _Tp&& __src, integral_constant<int, 3>) {
+  std::__construct_at(__dest, std::move(__src));
+  return *__dest;
+}
+
+template <class _Tp>
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 _Tp& __assign_trivially_copyable(_Tp* __dest, _Tp&& __src) {
+  static_assert(is_trivially_copyable<_Tp>::value,
+                "This function requires the type to be trivially copyable, otherwise we are not guaranteed that "
+                "we can assign to it using any of the methods used here.");
+  // clang-format off
+  return std::__assign_trivially_copyable_impl(__dest, std::move(__src), integral_constant<int,
+          is_copy_assignable<_Tp>::value    ? 0 :
+          is_move_assignable<_Tp>::value    ? 1 :
+          is_copy_constructible<_Tp>::value ? 2 :
+          is_move_constructible<_Tp>::value ? 3 : -1>());
+  // clang-format on
+}
+
 template <class _Tp, class _Up, __enable_if_t<__is_always_bitcastable<_Up, _Tp>::value, int> = 0>
 _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX14 _Tp*
 __constexpr_memmove(_Tp* __dest, _Up* __src, __element_count __n) {
@@ -144,12 +201,17 @@ __constexpr_memmove(_Tp* __dest, _Up* __src, __element_count __n) {
       return __dest;
     }
 #endif
+    // This basically implements __builtin_memmove in a constexpr-friendly way.
+    // We materialize a _Tp object from each _Up object (which we're allowed to
+    // because the types are bit-castable), and then we perform an "assignment"
+    // of the TriviallyCopyable _Tp object without relying on syntax that
+    // TriviallyCopyable doesn't guarantee we can use.
     if (std::__is_pointer_in_range(__src, __src + __count, __dest)) {
       for (; __count > 0; --__count)
-        __dest[__count - 1] = __src[__count - 1];
+        std::__assign_trivially_copyable(__dest + (__count - 1), __builtin_bit_cast(_Tp, __src[__count - 1]));
     } else {
       for (size_t __i = 0; __i != __count; ++__i)
-        __dest[__i] = __src[__i];
+        std::__assign_trivially_copyable(__dest + __i, __builtin_bit_cast(_Tp, __src[__i]));
     }
   } else if (__count > 0) {
     ::__builtin_memmove(__dest, __src, (__count - 1) * sizeof(_Tp) + __libcpp_datasizeof<_Tp>::value);
