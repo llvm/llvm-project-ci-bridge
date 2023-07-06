2fac84be8c3d08022aff18a552b29118d4bc6302
Louis Dionne <ldionne.2@gmail.com>
2023-07-06 10:27:54 -0400

    [libc++] Fix std::move algorithm with trivial move-only types
    As reported in https://reviews.llvm.org/D151953#4472195, the std::move
    algorithm (and various other functions that relied on it) stopped working
    after starting to use `__constexpr_memmove` in its implementation. This
    patch fixes the underlying issue in `__constexpr_memmove` and adds tests
    for various related algorithms and functions that were not exercising
    trivial move-only types.
    
    Differential Revision: https://reviews.llvm.org/D154613

--- libcxx/test/std/containers/sequences/vector/vector.modifiers/erase_iter.pass.cpp
+++ libcxx/test/std/containers/sequences/vector/vector.modifiers/erase_iter.pass.cpp
@@ -14,9 +14,10 @@
 #include <iterator>
 #include <cassert>
 
-#include "test_macros.h"
-#include "min_allocator.h"
 #include "asan_testing.h"
+#include "min_allocator.h"
+#include "MoveOnly.h"
+#include "test_macros.h"
 
 #ifndef TEST_HAS_NO_EXCEPTIONS
 struct Throws {
@@ -28,7 +29,7 @@ struct Throws {
     Throws& operator=(      Throws &&rhs) { v_ = rhs.v_; return *this; }
     int v_;
     static bool sThrows;
-    };
+};
 
 bool Throws::sThrows = false;
 #endif
@@ -36,70 +37,92 @@ bool Throws::sThrows = false;
 TEST_CONSTEXPR_CXX20 bool tests()
 {
     {
-    int a1[] = {1, 2, 3, 4, 5};
-    std::vector<int> l1(a1, a1+5);
-    l1.erase(l1.begin());
-    assert(is_contiguous_container_asan_correct(l1));
-    assert(l1 == std::vector<int>(a1+1, a1+5));
+        int a1[] = {1, 2, 3, 4, 5};
+        std::vector<int> l1(a1, a1+5);
+        l1.erase(l1.begin());
+        assert(is_contiguous_container_asan_correct(l1));
+        assert(l1 == std::vector<int>(a1+1, a1+5));
     }
     {
-    int a1[] = {1, 2, 3, 4, 5};
-    int e1[] = {1, 3, 4, 5};
-    std::vector<int> l1(a1, a1+5);
-    l1.erase(l1.begin() + 1);
-    assert(is_contiguous_container_asan_correct(l1));
-    assert(l1 == std::vector<int>(e1, e1+4));
+        int a1[] = {1, 2, 3, 4, 5};
+        int e1[] = {1, 3, 4, 5};
+        std::vector<int> l1(a1, a1+5);
+        l1.erase(l1.begin() + 1);
+        assert(is_contiguous_container_asan_correct(l1));
+        assert(l1 == std::vector<int>(e1, e1+4));
     }
     {
-    int a1[] = {1, 2, 3};
-    std::vector<int> l1(a1, a1+3);
-    std::vector<int>::const_iterator i = l1.begin();
-    assert(is_contiguous_container_asan_correct(l1));
-    ++i;
-    std::vector<int>::iterator j = l1.erase(i);
-    assert(l1.size() == 2);
-    assert(std::distance(l1.begin(), l1.end()) == 2);
-    assert(*j == 3);
-    assert(*l1.begin() == 1);
-    assert(*std::next(l1.begin()) == 3);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(j);
-    assert(j == l1.end());
-    assert(l1.size() == 1);
-    assert(std::distance(l1.begin(), l1.end()) == 1);
-    assert(*l1.begin() == 1);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(l1.begin());
-    assert(j == l1.end());
-    assert(l1.size() == 0);
-    assert(std::distance(l1.begin(), l1.end()) == 0);
-    assert(is_contiguous_container_asan_correct(l1));
+        int a1[] = {1, 2, 3};
+        std::vector<int> l1(a1, a1+3);
+        std::vector<int>::const_iterator i = l1.begin();
+        assert(is_contiguous_container_asan_correct(l1));
+        ++i;
+        std::vector<int>::iterator j = l1.erase(i);
+        assert(l1.size() == 2);
+        assert(std::distance(l1.begin(), l1.end()) == 2);
+        assert(*j == 3);
+        assert(*l1.begin() == 1);
+        assert(*std::next(l1.begin()) == 3);
+        assert(is_contiguous_container_asan_correct(l1));
+        j = l1.erase(j);
+        assert(j == l1.end());
+        assert(l1.size() == 1);
+        assert(std::distance(l1.begin(), l1.end()) == 1);
+        assert(*l1.begin() == 1);
+        assert(is_contiguous_container_asan_correct(l1));
+        j = l1.erase(l1.begin());
+        assert(j == l1.end());
+        assert(l1.size() == 0);
+        assert(std::distance(l1.begin(), l1.end()) == 0);
+        assert(is_contiguous_container_asan_correct(l1));
     }
+    // Make sure vector::erase works with move-only types
+    {
+        // When non-trivial
+        {
+            std::vector<MoveOnly> v;
+            v.emplace_back(1); v.emplace_back(2); v.emplace_back(3);
+            v.erase(v.begin());
+            assert(v.size() == 2);
+            assert(v[0] == MoveOnly(2));
+            assert(v[1] == MoveOnly(3));
+        }
+        // When trivial
+        {
+            std::vector<TrivialMoveOnly> v;
+            v.emplace_back(1); v.emplace_back(2); v.emplace_back(3);
+            v.erase(v.begin());
+            assert(v.size() == 2);
+            assert(v[0] == TrivialMoveOnly(2));
+            assert(v[1] == TrivialMoveOnly(3));
+        }
+    }
+
 #if TEST_STD_VER >= 11
     {
-    int a1[] = {1, 2, 3};
-    std::vector<int, min_allocator<int>> l1(a1, a1+3);
-    std::vector<int, min_allocator<int>>::const_iterator i = l1.begin();
-    assert(is_contiguous_container_asan_correct(l1));
-    ++i;
-    std::vector<int, min_allocator<int>>::iterator j = l1.erase(i);
-    assert(l1.size() == 2);
-    assert(std::distance(l1.begin(), l1.end()) == 2);
-    assert(*j == 3);
-    assert(*l1.begin() == 1);
-    assert(*std::next(l1.begin()) == 3);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(j);
-    assert(j == l1.end());
-    assert(l1.size() == 1);
-    assert(std::distance(l1.begin(), l1.end()) == 1);
-    assert(*l1.begin() == 1);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(l1.begin());
-    assert(j == l1.end());
-    assert(l1.size() == 0);
-    assert(std::distance(l1.begin(), l1.end()) == 0);
-    assert(is_contiguous_container_asan_correct(l1));
+        int a1[] = {1, 2, 3};
+        std::vector<int, min_allocator<int>> l1(a1, a1+3);
+        std::vector<int, min_allocator<int>>::const_iterator i = l1.begin();
+        assert(is_contiguous_container_asan_correct(l1));
+        ++i;
+        std::vector<int, min_allocator<int>>::iterator j = l1.erase(i);
+        assert(l1.size() == 2);
+        assert(std::distance(l1.begin(), l1.end()) == 2);
+        assert(*j == 3);
+        assert(*l1.begin() == 1);
+        assert(*std::next(l1.begin()) == 3);
+        assert(is_contiguous_container_asan_correct(l1));
+        j = l1.erase(j);
+        assert(j == l1.end());
+        assert(l1.size() == 1);
+        assert(std::distance(l1.begin(), l1.end()) == 1);
+        assert(*l1.begin() == 1);
+        assert(is_contiguous_container_asan_correct(l1));
+        j = l1.erase(l1.begin());
+        assert(j == l1.end());
+        assert(l1.size() == 0);
+        assert(std::distance(l1.begin(), l1.end()) == 0);
+        assert(is_contiguous_container_asan_correct(l1));
     }
 #endif
 
